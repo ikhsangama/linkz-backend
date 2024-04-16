013e50997652ef9b1698ace92a90eed222b1e586
ikhsangama <ikhsangama@gmail.com>
2024-04-16 22:09:09 +0700

    feat: test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,18 +2,66 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { EntityManager } from '@mikro-orm/postgresql';
 import { User } from './user.entity';
+import { FirebaseService } from '../firebase/firebase.service';
+
+function MockFirebaseService() {
+  // Return a mock instance of your FirebaseService
+  let uid = null;
+
+  return {
+    getAuth: () => ({
+      getUser: (uid) => {
+        return new Promise((resolve, reject) => {
+          if (uid === null) {
+            reject('no uid');
+          } else {
+            resolve({ uid });
+          }
+        });
+      }
+    }),
+
+    setUid: (newUid) => {
+      uid = newUid;
+    },
+  }
+}
+
+expect.extend({
+  toBeWithinSecondsOf(received, expected, seconds = 1) {
+    const diff = Math.abs(received.getTime() - expected.getTime()) / 1000;
+    const pass = diff < seconds;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be within ${seconds} seconds of ${expected}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be within ${seconds} seconds of ${expected}`,
+        pass: false,
+      };
+    }
+  }
+});
 
 describe('UserService', () => {
   let userService: UserService;
+  let mockFirebaseService;
+
   const mockEntityManager = {
     findOne: jest.fn(),
     persistAndFlush: jest.fn(),
   };
 
   beforeEach(async () => {
+    mockFirebaseService = MockFirebaseService();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
+        { provide: FirebaseService, useValue: mockFirebaseService },
         { provide: EntityManager, useValue: mockEntityManager },
       ],
     }).compile();
@@ -24,11 +72,16 @@ describe('UserService', () => {
   describe('register', () => {
     it('should register a new user', async () => {
       const newUser = new User();
-      newUser.uid = 'uid';
+      newUser.uid = 'valid-uid';
 
-      await userService.register('uid');
+      mockFirebaseService.setUid('valid-uid');
+      await userService.register('valid-uid');
 
-      expect(mockEntityManager.persistAndFlush).toHaveBeenCalledWith(newUser);
+      expect(mockEntityManager.persistAndFlush).toHaveBeenCalledWith(
+        expect.objectContaining({
+          uid: 'valid-uid',
+        })
+      );
     });
   });
 
